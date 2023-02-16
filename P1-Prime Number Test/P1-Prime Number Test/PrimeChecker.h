d0ec24cd1f2b667fabf395e769835de4a2055706
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2023-02-16 22:00:04 +0800

    Update PrimeChecker.h

--- P1-Prime Number Test/P1-Prime Number Test/PrimeChecker.h
+++ P1-Prime Number Test/P1-Prime Number Test/PrimeChecker.h
@@ -5,11 +5,12 @@ class PrimeChecker : public IETThread
 public:
 	PrimeChecker(int id, int interval, int given);
 	int id;
+	int interval;
+	int givenNumber;
+	bool isPrime = false;
+	bool isFinished = false;
 	// Inherited via IETThread
 	virtual void run() override;
 
-private:
-	
-	int in
 };
 
