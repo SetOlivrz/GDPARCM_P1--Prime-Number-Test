785cf36a794b1b0ef50232c6e21709347fa8a41c
KyleDesquitado <kyle_francis_desquitado@dlsu.edu.ph>
2023-02-16 21:46:43 +0800

    prime checker thread
    v0.1

---
+++ P1-Prime Number Test/P1-Prime Number Test/PrimeChecker.cpp
@@ -0,0 +1,11 @@
+#include "PrimeChecker.h"
+
+void PrimeChecker::run()
+{
+	int currentValue = 2 + id;
+	//for(int i = 0; i < )
+
+	do {}
+		while(currentValue <= givenNumber)
+
+}
--- P1-Prime Number Test/P1-Prime Number Test/PrimeChecker.h
+++ P1-Prime Number Test/P1-Prime Number Test/PrimeChecker.h
@@ -2,7 +2,14 @@
 #include "IETThread.h"
 class PrimeChecker : public IETThread
 {
+public:
+	PrimeChecker(int id, int interval, int given);
+	int id;
 	// Inherited via IETThread
 	virtual void run() override;
+
+private:
+	
+	int in
 };
 
